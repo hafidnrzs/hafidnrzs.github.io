48431acab41cd027b6730ebf52aba8bd8795cf29
Hafid Nur <hafidnurazis@gmail.com>
2025-01-20 22:52:34 +0700

    chore(deploy): fix static files not loaded on deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,4 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://hafidnrzs.github.io',
-    base: 'hafidnrzs.github.io',
 });
