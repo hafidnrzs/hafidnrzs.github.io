d923dd9c6873d46169074bfde21eaaa06a1ad1cd
Hafid Nur <hafidnurazis@gmail.com>
2025-01-20 22:41:24 +0700

    chore: setup GitHub Action for deployment to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://hafidnrzs.github.io/hafidnrzs.github.io',
-    base: '/hafidnrzs.github.io/',
-    output: 'static',
+    site: 'https://hafidnrzs.github.io',
+    base: 'hafidnrzs.github.io',
 });
