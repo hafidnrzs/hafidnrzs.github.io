4fce8d487564b89f509cac040ec377307f89cc2c
Hafid Nur <hafidnurazis@gmail.com>
2025-01-20 22:26:59 +0700

    build: configure Github Pages for Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://hafidnrzs.github.io/hafidnrzs.github.io',
+    base: '/hafidnrzs.github.io/',
+    output: 'static',
+});
