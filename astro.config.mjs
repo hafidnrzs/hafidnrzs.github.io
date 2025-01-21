247ff2c9dc66ab578cc2b090db473933dfebcf76
Hafid Nur <hafidnurazis@gmail.com>
2025-01-22 00:50:01 +0700

    chore: add TailwindCSS integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
 export default defineConfig({
-    // site: 'https://hafidnrzs.github.io',
-    site: 'https://me.hafidnrzs.com',
+  // site: 'https://hafidnrzs.github.io',
+  site: 'https://me.hafidnrzs.com',
+
+  integrations: [tailwind()],
 });
