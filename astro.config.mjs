3351c8df4a1447a775809ce6e8c6261c194cc481
hafidnrzs <hafidnurazis@gmail.com>
2025-09-14 14:32:28 +0700

    feat!: new landing page
    - added new landing page components: Navbar, Hero, and Footer.
    - using astro-icon and Iconify for icons.
    - import necessary fonts.
    - added placeholder section for future content.
    - config prettier and Astro integration for TailwindCSS & astro-icon.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
-
+import icon from 'astro-icon';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   site: 'https://hafidnrzs.com',
-
+  integrations: [icon()],
   vite: {
     plugins: [tailwindcss()],
   },
